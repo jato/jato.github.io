7e3b6f151d35a2cf366cdb87619ef74d4d234411
jato <jatobrown@gmail.com>
2016-07-25 17:04:27 -0700

    [Projects] Adds color transitions

--- js/landing.js
+++ js/landing.js
@@ -8,31 +8,31 @@ $(document).ready(function(){
     $("section #services li").fadeIn(2000);
     // $("footer").hide();
     // $("footer").fadeIn(2000);
-    $("footer p").mouseover(function() {
-      $(this).animate({ color: '#f00'},500);
-  }).mouseout(function() {
-      $(this).animate({ color: '#ccc'},500);
-  });
+  //   $("footer p").mouseover(function() {
+  //     $(this).animate({ color: '#f00'},500);
+  // }).mouseout(function() {
+  //     $(this).animate({ color: '#ccc'},500);
+  // });
 
 
-  $('#proj-id').click(function(e) {
-    e.preventDefault(); 
+  // $('#proj-id').click(function(e) {
+  //   e.preventDefault(); 
 
-    $('#content').fadeToggle(900, 'linear');
+  //   $('#content').fadeToggle(900, 'linear');
 
-      $('html, body').animate({scrollTop:$('#content').position().top}, 'slow');
+  //     $('html, body').animate({scrollTop:$('#content').position().top}, 'slow');
       
-    // var pageToLoad = $(this).attr('href'); // gets the href of the clicked link
-    // $('#content').load(pageToLoad); // loads the remote page into the content div without refresh
+  //   // var pageToLoad = $(this).attr('href'); // gets the href of the clicked link
+  //   // $('#content').load(pageToLoad); // loads the remote page into the content div without refresh
 
-    // $.ajax({ 
-    //  url: "projects/project_index.html", dataType: "html" 
-    // }).done(function( responseHtml ) {
-    //  $("#content").html(responseHtml);
-    // });
+  //   // $.ajax({ 
+  //   //  url: "projects/project_index.html", dataType: "html" 
+  //   // }).done(function( responseHtml ) {
+  //   //  $("#content").html(responseHtml);
+  //   // });
 
-    // $('head').append('<link rel="stylesheet" type="text/css" href="stylesheets/projects.css"/>');
-    });
+  //   // $('head').append('<link rel="stylesheet" type="text/css" href="stylesheets/projects.css"/>');
+  //   });
 
         // $('a[href*=#]:not([href=#])').click(function() {
         //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
